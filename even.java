7ccafcf632eeb56c308ac09fa7c6c0226a343fcd
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2018-10-05 07:24:02 -0500

    Create even.java

---
+++ even.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{   
+		int n,i,s;
+		Scanner a=new Scanner(System.in);
+		s=a.nextInt();
+		n=a.nextInt();
+		for(i=s+1;i<n;i++)
+		{
+			if(i%2==0)
+	      	{
+	      	System.out.println(i);
+		}}
+		
+		}
+}
