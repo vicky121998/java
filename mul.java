1f67cb51e6cd9d84fcb6369466cb08eb9aef7eec
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2018-10-07 10:32:25 -0400

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int f=1,n,i;
+Scanner a=new Scanner(System.in);
+n=a.nextInt();
+for(i=1;i<=5;i++)
+	{
+		f=n*i;
+	System.out.println(f);
+		}
+	}
+}
