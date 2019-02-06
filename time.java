6e76705464f4245da8afa2340dd9a5dee31ba3c7
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-02-06 18:25:42 +0530

    Create time.java

---
+++ time.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args)
+	{
+	int h,m;
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	h=n/60;
+	m=n%60;
+	System.out.println("\n"+h+"\t"+m);
+	
+	}
+}
