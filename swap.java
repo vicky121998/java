b7e42d7d23cb431894e09748ede705e811a9f7d2
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-02-17 10:57:05 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,l,w=0;
+Scanner v=new Scanner(System.in);
+n=v.nextInt();
+l=v.nextInt();	
+w=n;
+n=l;
+l=w;
+	
+		System.out.println(n+"\t"+l);
+	}
+}
