8e9de2150b9e89173b1d1a13d22b41f287ce3712
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-02-17 10:19:02 +0530

    Create lcount.java

---
+++ lcount.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,c=0;
+		String s;
+		Scanner v=new Scanner(System.in);
+		s=v.nextLine();
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='.')
+		{
+			c++;
+		}	
+		}
+	
+		System.out.println(c+1);
+	}
+}
