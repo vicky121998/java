dc5f568c8003bf1e3b0ff926be003c94788d2b91
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-02-24 10:49:41 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s;
+		int n;
+		Scanner v=new Scanner(System.in);
+		s=v.next();
+		n=v.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(s);
+		}
+	}
+}
