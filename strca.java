3cd35e31b66aef9725722c68c9d7418569e90107
vicky121998 <36404884+vicky121998@users.noreply.github.com>
2019-02-24 11:24:51 +0530

    Create strca.java

---
+++ strca.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class vk
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s1,s2;
+		Scanner x=new Scanner(System.in);
+		s1=x.next();
+		s2=x.next();
+			System.out.println(s1+s2);
+		}
+}
